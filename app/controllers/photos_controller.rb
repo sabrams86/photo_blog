8ef13bde20dba001cbeeb94c279f23dddf35d445
Steven Abrams <steven.abrams86@gmail.com>
2016-05-27 18:48:12 -0600

    photos crud

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,38 @@
 class PhotosController < ApplicationController
 
   def index
+    @photos = Photo.all
   end
 
   def new
-    Photo.new
+    @photo = Photo.new
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to(@photo)
+    else
+      render new
+    end
+  end
+
+  def destroy
+    if Photo.find(params[:id]).destroy
+      redirect_to photos_path
+    else
+      redirect_to(@photo)
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :name)
   end
 
 end
